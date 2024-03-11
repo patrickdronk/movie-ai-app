34e122312e90ad6b4792e87cb9b4d7501a6f5b7e
patrickdronk <patrickdronk@me.com>
2024-03-11 23:40:29 +0100

    Update login.jsx

--- src/routes/login.jsx
+++ src/routes/login.jsx
@@ -17,7 +17,7 @@ export default function Login() {
 
   useEffect(
     () =>
-      hanko.onAuthFlowCompleted(async () => {
+      hanko.onSessionCreated(async () => {
         await redirectAfterLogin();
       }),
     [hanko, redirectAfterLogin]
